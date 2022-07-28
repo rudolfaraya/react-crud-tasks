354c53ff385221098e79b46eddf722678ba37588
Rodolfo Araya <raraya@4talent.cl>
2022-07-28 17:46:55 -0400

    Add Redux and create store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import {useSelector} from 'react-redux'
 
 function App() {
-  return (
+    const tasksState = useSelector(state => state.tasks)
+    console.log(tasksState)
+
+    return (
     <div className="App">
       <h1>Hello World</h1>
     </div>
-  );
+);
 }
 
 export default App;
